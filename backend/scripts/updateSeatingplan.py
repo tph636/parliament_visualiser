513f02eb8563d8be4dd2e85f414109efe89ccd85
karanka <kar4nka@protonmail.com>
2024-12-16 00:30:52 +0200

    Add updateSeatingplan.py which fetches seating plan of parliament and creates a table for it.

---
+++ backend/scripts/updateSeatingplan.py
@@ -0,0 +1,56 @@
+import requests
+import json
+import sqlite3
+import os 
+
+# Fetch seating plan data
+def fetch_data(page):
+    url = f"https://avoindata.eduskunta.fi/api/v1/tables/SeatingOfParliament/rows?perPage=100&page={page}"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+# It is not possible to get the seats for all 200 parliament members at once
+# so we have to do it in two parts
+data_page_0 = fetch_data(0)
+data_page_1 = fetch_data(1)
+
+# Combine rowData from both pages
+combined_data = {
+    "tableName": data_page_0["tableName"],
+    "columnNames": data_page_0["columnNames"],
+    "rowData": data_page_0["rowData"] + data_page_1["rowData"],  # Combine rowData
+}
+
+#print(json.dumps(combined_data, indent=4, ensure_ascii=False))
+
+db_path = os.path.join(os.path.dirname(__file__), '..', 'databases', 'database.db')
+
+os.makedirs(os.path.dirname(db_path), exist_ok=True)
+conn = sqlite3.connect(db_path)
+cursor = conn.cursor()
+
+# Drop the table if it already exists
+cursor.execute('DROP TABLE IF EXISTS SeatingOfParliament')
+
+# Create the seatingplan table with appropriate columns
+cursor.execute(f'''
+               CREATE TABLE SeatingOfParliament (
+               hetekaId INTEGER PRIMARY KEY,
+               seatNumber INTEGER,
+               lastname TEXT,
+               firstname TEXT,
+               party TEXT,
+               minister TEXT
+               )''')
+
+# Insert combined data into the SeatingOfParliament table
+
+for row in combined_data["rowData"]:
+    cursor.execute('''
+                   INSERT INTO SeatingOfParliament (hetekaId, seatNumber, lastname, firstname, party, minister)
+                   VALUES (?, ?, ?, ?, ?, ?)''', (row[0], row[1], row[2], row[3], row[4], row[5]))
+
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
