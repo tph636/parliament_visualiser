b08281609397644ff6a3601d9f45fb4fe4ae5d08
karanka <kar4nka@protonmail.com>
2025-07-18 18:16:10 +0300

    Add PersonInfo component

--- remix-app/app/routes/$personId.jsx
+++ remix-app/app/routes/$personId.jsx
@@ -1,40 +1,27 @@
 import { useLoaderData } from "@remix-run/react";
+import PersonInfo from "../components/PersonInfo/PersonInfo";
 
-// Loader function to fetch member data
 export const loader = async ({ params }) => {
   const baseURL = process.env.INTERNAL_BACKEND_API_URL;
-
   const memberResponse = await fetch(`${baseURL}/api/member_of_parliament/${params.personId}`);
-  
+
   if (!memberResponse.ok) {
     throw new Response("Not Found", { status: 404 });
   }
+
   const member = await memberResponse.json();
   return { member };
 };
 
-export const headers = () => { 
-  return { 
-    "Cache-Control": "max-age=3600"
-  };
-};
+export const headers = () => ({
+  "Cache-Control": "max-age=3600"
+});
 
-export default function MemberInfo() {
+export default function MemberPage() {
   const { member } = useLoaderData();
-
   return (
-    <div className="member-info">
-      <img
-        src={`${import.meta.env.VITE_BACKEND_API_URL}/images/high-res/${member.image}`}
-        alt={`Edustajan ${member.firstname} ${member.lastname} kuva`}
-        className="member-image"
-        style={{
-          border: `4px solid ${member.party_color}`
-        }}
-      />
-      <h2>{member.firstname} {member.lastname}</h2>
-      <p>Eduskuntaryhmä: {member.parliament_group}</p>
-      <p>Välihuutoja: {member.valihuuto_count}</p>
-    </div>
+    <>
+      <PersonInfo member={member} />
+    </>
   );
 }
