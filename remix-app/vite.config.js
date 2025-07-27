ac76adf818ed25088a86083e7d86a23464cc601f
karanka <kar4nka@protonmail.com>
2025-07-27 17:58:49 +0300

    Update vite.config.js to prepare for RR7 migration

--- remix-app/vite.config.js
+++ remix-app/vite.config.js
@@ -1,5 +1,6 @@
-import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import { reactRouter } from "@react-router/dev/vite";
+import tsconfigPaths from "vite-tsconfig-paths"; // optional, if you're using path aliases
 
 // Load env variables
 const PORT = parseInt(process.env.PORT, 10);
@@ -9,14 +10,7 @@ export default defineConfig({
     port: PORT,
   },
   plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
+    reactRouter(),
+    tsconfigPaths(), // Optional but recommended for path alias support
   ],
 });
